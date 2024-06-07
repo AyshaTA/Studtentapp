cc35a4a7616afd869b2aa6fff8cac10865b6bbe8
Aysha T A <ayshata2019@gmail.com>
2024-06-07 15:10:48 +0530

    completed

--- src/components/NavStudent.jsx
+++ src/components/NavStudent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavStudent = () => {
     return (
@@ -6,27 +7,25 @@ const NavStudent = () => {
 
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">STUDENT APP</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/search">Search</a>
+                                <Link class="nav-link active" aria-current="page" to="/search">Search</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/delete">Delete</a>
+                                <Link class="nav-link active" aria-current="page" to="/delete">Delete</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/viewallstudent">View All Student</a>
+                                <Link class="nav-link" to="/viewallstudent">View All Student</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
+                            
                         </ul>
                     </div>
                 </div>
